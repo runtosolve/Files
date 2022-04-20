2943ac6483f5337e7d7b124bb763da3fd0c8e095
Cris Moen <cris.moen@runtosolve.com>
2022-04-20 12:33:14 -0400

    for JuliaHub

---
+++ TH/Pluto_test.jl
@@ -0,0 +1,36 @@
+### A Pluto.jl notebook ###
+# v0.18.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 02893d00-c0c7-11ec-02d8-3bcf052947b9
+x=1
+
+# ╔═╡ 2cc0fd10-af66-4e8b-baea-7664302a2915
+y=1
+
+# ╔═╡ db5149e8-caa6-42df-a715-14cb1bd840ef
+x+y
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═02893d00-c0c7-11ec-02d8-3bcf052947b9
+# ╠═2cc0fd10-af66-4e8b-baea-7664302a2915
+# ╠═db5149e8-caa6-42df-a715-14cb1bd840ef
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
